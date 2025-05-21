e33612753403ee311cfd05803cc355e43d5e1b2f
osaidhaj03 <osaidhaj03@gmail.com>
2025-05-22 00:38:06 +0300

    Restore Meta Info section in Contract1Resource form with hired_date and hired_by fields

--- app/Filament/Resources/Contract1Resource.php
+++ app/Filament/Resources/Contract1Resource.php
@@ -102,14 +102,14 @@ class Contract1Resource extends Resource
          //       SignaturePad::make('witness1_signature')->label('Witness 1 Signature')->required(),
          //       SignaturePad::make('witness2_signature')->label('Witness 2 Signature')->required(),
          //   ])->columns(4),
-//
-       //     Forms\Components\Section::make('Meta Info')->schema([
-       //         Forms\Components\DatePicker::make('hired_date')
-       //             ->default(now())->readOnly(),
-       //         Forms\Components\TextInput::make('hired_by')
-       //             ->default(fn () => Auth::user()?->name)
-       //             ->readOnly(),
-       //     ])->columns(2),
+
+            Forms\Components\Section::make('Meta Info')->schema([
+                Forms\Components\DatePicker::make('hired_date')
+                    ->default(now())->readOnly(),
+                Forms\Components\TextInput::make('hired_by')
+                    ->default(fn () => Auth::user()?->name)
+                    ->readOnly(),
+            ])->columns(2),
         ]);
     }
 
