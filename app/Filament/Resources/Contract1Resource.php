2b765409fc085a580c5a3d8e38350dd319c8ea9a
osaidhaj03 <osaidhaj03@gmail.com>
2025-05-22 00:40:17 +0300

    Add missing Auth facade import in Contract1Resource

--- app/Filament/Resources/Contract1Resource.php
+++ app/Filament/Resources/Contract1Resource.php
@@ -12,6 +12,7 @@ use Filament\Resources\Resource;
 use Filament\Forms\Form;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\Auth;
 
 //use Saade\FilamentAutograph\Forms\Components\SignaturePad;
 
