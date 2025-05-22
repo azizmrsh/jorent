4d9ae08664dffd3b38900a987077fda417298599
osaidhaj03 <osaidhaj03@gmail.com>
2025-05-22 23:38:27 +0300

    Change signature fields from longText to string in Contract1Resource migration

--- database/migrations/2025_05_21_082519_create_contract1s_table.php
+++ database/migrations/2025_05_21_082519_create_contract1s_table.php
@@ -26,10 +26,10 @@ return new class extends Migration
             $table->text('terms_and_conditions_extra')->nullable();
 
             // توقيعات
-            $table->longText('tenant_signature')->nullable();
-            $table->longText('landlord_signature')->nullable();
-            $table->longText('witness1_signature')->nullable();
-            $table->longText('witness2_signature')->nullable();
+            $table->string('tenant_signature')->nullable();
+            $table->string('landlord_signature')->nullable();
+            $table->string('witness1_signature')->nullable();
+            $table->string('witness2_signature')->nullable();
 
             // بيانات التوظيف
             $table->date('hired_date')->nullable();
