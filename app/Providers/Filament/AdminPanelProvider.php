09cdf50ae2059e8985f7249d7a767bac1f16df5a
osaidhaj03 <osaidhaj03@gmail.com>
2025-05-25 00:01:34 +0300

    Remove widget definitions from AdminPanelProvider to simplify panel configuration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,21 +36,7 @@ class AdminPanelProvider extends PanelProvider
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                // ترتيب widgets بحيث تكون الإحصائيات والرسوم البيانية في الأعلى
-                Widgets\AccountWidget::class,
-                \App\Filament\Widgets\StatsOverviewWidget::class,
-                \App\Filament\Widgets\PaymentStatsWidget::class,
-                \App\Filament\Widgets\ContractsChartWidget::class,
-                \App\Filament\Widgets\ContractsByStatusWidget::class, 
-                
-                // الجداول في الأسفل
-                \App\Filament\Widgets\LatestContractsWidget::class,
-                \App\Filament\Widgets\TenantsTableWidget::class,
-                
-                // معلومات النظام
-                Widgets\FilamentInfoWidget::class,
-            ])
+            
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
